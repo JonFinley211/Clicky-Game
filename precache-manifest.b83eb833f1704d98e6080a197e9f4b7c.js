4f69a862459ecc9f10875e7ecb44eb9c70ecdcf5
Jon Finley <lilfin19@gmail.com>
2019-12-31 16:02:18 -0600

    Updates

---
+++ precache-manifest.b83eb833f1704d98e6080a197e9f4b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4635588b99dde700981dcce1b15f22e",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "e4cc5db14fcd85c1547f",
+    "url": "/Clicky-Game/static/css/main.1baa31a7.chunk.css"
+  },
+  {
+    "revision": "dec94ecd482061595b10",
+    "url": "/Clicky-Game/static/js/2.8bde30e0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.8bde30e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4cc5db14fcd85c1547f",
+    "url": "/Clicky-Game/static/js/main.ea97d0eb.chunk.js"
+  },
+  {
+    "revision": "1f0b6c9876191dbe70e1",
+    "url": "/Clicky-Game/static/js/runtime-main.3eae3300.js"
+  }
+]);
